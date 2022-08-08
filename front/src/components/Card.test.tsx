f621f3ac1abdce32199a853fcdc0719e869b5cf9
Fabio Jun <fabiojundev@gmail.com>
2022-08-08 18:38:18 -0300

    Add card component and tests

---
+++ front/src/components/Card.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import Card from "./Card";
+import ICard from "../types/card.type";
+
+const card: ICard = {
+  titulo: "Titulo",
+  conteudo: "Conteudo",
+  lista: "todo",
+};
+
+afterEach(cleanup);
+
+describe("Card", () => {
+  const setCard = jest.fn();
+
+  it("Renders Card", () => {
+    render(<Card card={card} setCard={setCard} />);
+    const title = screen.getByPlaceholderText(/Título/i);
+    expect(title).toHaveValue(card.titulo);
+
+    const conteudo = screen.getByPlaceholderText(/Conteúdo/i);
+    expect(conteudo).toHaveValue(card.conteudo);
+
+    const lista = screen.getByPlaceholderText(/Lista/i);
+    expect(lista).toHaveValue(card.lista);
+  });
+
+  it("Renders Card with empty list", () => {
+    card.lista = "";
+    render(<Card card={card} setCard={setCard} />);
+    const title = screen.getByPlaceholderText(/Título/i);
+    expect(title).toHaveValue(card.titulo);
+
+    const conteudo = screen.getByPlaceholderText(/Conteúdo/i);
+    expect(conteudo).toHaveValue(card.conteudo);
+
+    expect(screen.queryByPlaceholderText(/Lista/i)).not.toBeInTheDocument();
+  });
+});
---
+++ front/src/components/Card.tsx
@@ -0,0 +1,36 @@
+import ICard from "../types/card.type";
+
+function Card({
+  card,
+  setCard,
+}: {
+  card: ICard;
+  setCard: (card: ICard) => ICard;
+}) {
+  return (
+    <div className="Card">
+      <input
+        type="text"
+        placeholder="Título"
+        value={card.titulo}
+        onChange={(e) => setCard({ ...card, titulo: e.target.value })}
+      />
+      <input
+        type="textarea"
+        placeholder="Conteúdo"
+        value={card.conteudo}
+        onChange={(e) => setCard({ ...card, conteudo: e.target.value })}
+      />
+      {card.lista && (
+        <input
+          type="text"
+          placeholder="Lista"
+          value={card.lista}
+          onChange={(e) => setCard({ ...card, conteudo: e.target.value })}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Card;
